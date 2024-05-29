6d6dc1d45e71002117acc6073a147599a09ce6fa
Pedro Souza <pedro.souza4237@gmail.com>
2024-05-29 16:59:28 -0300

    Configuring Stitches

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,22 @@
+import { styled } from "../../styles";
+
+const Button = styled("button", {
+  backgroundColor: "$rocketseat",
+  color: "white",
+  padding: "10px 20px",
+  border: "none",
+  borderRadius: 6,
+
+  "&:hover": {
+    filter: "brightness(0.9)"
+  }
+})
 
 export default function Home() {
   return (
     <>
       <h1>Home</h1>
+      <Button>Click me</Button>
     </>
   );
 }
