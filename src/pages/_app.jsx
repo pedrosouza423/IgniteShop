c9b39f5db87a15533b4ff59f25d3dff9b5f0ffa5
Pedro Souza <pedro.souza4237@gmail.com>
2024-05-30 11:32:19 -0300

    Application header

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,8 +1,18 @@
 import { globalStyles } from "../../styles/global";
+// import logoImg from "../assets/logo.svg"
+import { Container, Header } from "../../styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(
+    <Container>
+      <Header>
+        {/* <img src={logoImg.src} alt="" /> */}
+        <p>LOGO</p>
+      </Header>
+      <Component {...pageProps} />
+    </Container>
+  )
 }
 
