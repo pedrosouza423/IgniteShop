8838236d1f153dbac1dbc0c13dbdededc306245a
Pedro Souza <pedro.souza4237@gmail.com>
2024-05-30 13:09:16 -0300

    Image next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import { globalStyles } from "../../styles/global";
-// import logoImg from "../assets/logo.svg"
+import Logo from "../assets/logo.svg"
 import { Container, Header } from "../../styles/pages/app";
+import Image  from 'next/image';
+
 
 globalStyles();
 
@@ -8,8 +10,7 @@ export default function App({ Component, pageProps }) {
   return(
     <Container>
       <Header>
-        {/* <img src={logoImg.src} alt="" /> */}
-        <p>LOGO</p>
+        <Image src={Logo} alt="Logo" />
       </Header>
       <Component {...pageProps} />
     </Container>
