9d73f78618b10efb7a942274b8296cbe101399a7
Pedro Souza <pedro.souza4237@gmail.com>
2024-06-01 18:33:37 -0300

    Home Structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,32 @@
+import Image from "next/image";
 import { styled } from "../../styles";
+import { HomeContainer, Product } from "../../styles/pages/home";
 
-const Button = styled("button", {
-  backgroundColor: "$green300",
-  color: "white",
-  padding: "10px 20px",
-  border: "none",
-  borderRadius: 6,
-
-  "&:hover": {
-    filter: "brightness(0.9)"
-  }
-})
+import camiseta1 from '../assets/camisetas/1.png'
+import camiseta2 from '../assets/camisetas/2.png'
+import camiseta3 from '../assets/camisetas/3.png'
+import camiseta4 from '../assets/camisetas/4.png'
 
 export default function Home() {
   return (
-    <>
-      <h1>Home</h1>
-      <Button>Click me</Button>
-    </>
+    <HomeContainer>
+      <Product>
+        <Image width={520} height={480} src={camiseta1} alt={""}        
+        />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image width={520} height={480} src={camiseta2} alt={""}        
+        />
+        <footer>
+          <strong>Camiseta Y</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
